96078976550f532a21e50d68595f2d8587d17ef6
Ricky Ying <rickycolluni@gmail.com>
2024-06-12 21:13:07 +0700

    Added basic human input
    Using while loop to ask the player if the input is not rock, paper, or scissor. Sometimes i think i should not have access to Javascript.

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-console.log("Hello World");
+let compScore;
+let humanScore;
+
+while (true) {
 
 function getComputerChoice() {
   let compChoice;
@@ -20,5 +23,23 @@ function getComputerChoice() {
   return compChoice;
 }
 
+console.log("Opponent has chosen");
 let opponent = getComputerChoice();
 console.log(opponent);
+
+function getHumanChoice() {
+  let incorrectInput = true;
+  while (incorrectInput) {
+    let answer = prompt("Please type in [rock], [paper], or [scissor]");
+    if (answer == "rock" || answer == "paper" || answer == "scissor") {
+      return answer;
+    }
+  }
+}
+
+let human = getHumanChoice();
+console.log(human);
+
+
+
+}
