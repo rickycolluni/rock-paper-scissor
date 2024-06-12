4cbf6f9f77a76684e9c1a38f902e4b3c9afabe1b
Ricky Ying <rickycolluni@gmail.com>
2024-06-12 20:20:28 +0700

    Added basic randomize opponent choice
    There has to be a better way to implement Math.random(). Use my implementation as an example of how not to.

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log("Hello World");
+
+function getComputerChoice() {
+  let compChoice;
+  let num;
+
+  num = Math.random() * (91 - 1) + 1;
+
+  switch (true) {
+    case num > 60 :
+      compChoice = "scissor";
+      break;
+    case num < 30 :
+      compChoice = "paper";
+      break;
+    default :
+      compChoice = "rock";
+  }
+
+  return compChoice;
+}
+
+let opponent = getComputerChoice();
+console.log(opponent);
