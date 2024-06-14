a11d2b8a14c8042030a728b2f3ac78bc933b04f4
Ricky Ying <rickycolluni@gmail.com>
2024-06-14 20:47:34 +0700

    Added basic score counting, win lose condition, and won/lost message.
    Once again, please use my implementation as what not to do. I am proud that i can make a somewhat working rock paper scissors game, but seriously, this looked like i was chasing a deadline.

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-let compScore;
-let humanScore;
-
-while (true) {
-
 function getComputerChoice() {
   let compChoice;
   let num;
@@ -23,10 +18,6 @@ function getComputerChoice() {
   return compChoice;
 }
 
-console.log("Opponent has chosen");
-let opponent = getComputerChoice();
-console.log(opponent);
-
 function getHumanChoice() {
   let incorrectInput = true;
   while (incorrectInput) {
@@ -37,9 +28,58 @@ function getHumanChoice() {
   }
 }
 
-let human = getHumanChoice();
-console.log(human);
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice == computerChoice) {
+    console.log("Draw! Both you and the computer chose " + humanChoice);
+  } else if (humanChoice == "rock") {
+    if (computerChoice == "paper") {
+        console.log("You Lose, you picked " + humanChoice + ", Computer chosen " + computerChoice);
+        compScore += 1;
+    } else {
+        console.log("You Win, you picked " + humanChoice + ", Computer chosen " + computerChoice);
+        humanScore += 1;
+    }
+  } else if (humanChoice == "paper") {
+    if (computerChoice == "scissor") {
+      console.log("You Lose, you picked " + humanChoice + ", Computer chosen " + computerChoice);
+      compScore += 1;
+    } else {
+      console.log("You Win, you picked " + humanChoice + ", Computer chosen " + computerChoice);
+      humanScore += 1;
+    }
+  } else if (humanChoice == "scissor") {
+    if (computerChoice == "rock") {
+      console.log("You Lose, you picked " + humanChoice + ", Computer chosen " + computerChoice);
+      compScore += 1;
+    } else {
+      console.log("You Win, you picked " + humanChoice + ", Computer chosen " + computerChoice);
+      humanScore += 1;
+    }
+  }
+}
 
+function playGame(amountOfGames) {
 
+  for (let i = 0; i < amountOfGames; i++) {
+    console.log("Opponent has chosen");
+    let opponent = getComputerChoice();
+    console.log(opponent);
+    let human = getHumanChoice();
+    console.log(human);
 
+    playRound(human, opponent);
+
+    console.log(humanScore + " vs " + compScore);
+  }
+}
+
+let compScore = 0;
+let humanScore = 0;
+playGame(5);
+if (humanScore > compScore) {
+  console.log("Congratulations, You won");
+} else if (humanScore < compScore) {
+  console.log("Too bad, You lost");
+} else {
+  console.log("Both You and the computer are tied");
 }
